6f2bb5a0ca211f622e907db109ed6fcaef63aec3
Axiid <devaxiid@gmail.com>
2023-04-02 12:06:49 +0200

    Adattata la classe main al ProviderService
    Took 5 minutes

--- src/main/java/it/enumproject/plugin/Enum.java
+++ src/main/java/it/enumproject/plugin/Enum.java
@@ -1,33 +1,25 @@
 package it.enumproject.plugin;
 
 import it.enumproject.libs.EnumPlugin;
-import it.enumproject.provider.EnumProvider;
 
 public class Enum extends EnumPlugin {
 
     private static Enum instance;
-    private EnumProvider provider;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         instance = this;
-        provider = new EnumProvider(this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
         instance = null;
-        provider = null;
     }
 
     public static Enum getInstance() {
         return instance;
-    }
-
-    public EnumProvider getProvider() {
-        return provider;
     }
 
 }
