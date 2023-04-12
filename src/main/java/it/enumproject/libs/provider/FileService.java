78bb27033c03eeeb499f81d860b9e3a799f2c6e6
Axiid <devaxiid@gmail.com>
2023-04-12 21:01:25 +0200

    Renamed FileUtils to FileService
    Took 6 minutes

---
+++ src/main/java/it/enumproject/libs/provider/FileService.java
@@ -0,0 +1,13 @@
+package it.enumproject.libs.provider;
+
+import it.enumproject.libs.EnumPlugin;
+
+public class FileService {
+
+    private final EnumPlugin instance;
+
+    public FileService(EnumPlugin instance, String fileName) {
+        this.instance = instance;
+    }
+
+}
