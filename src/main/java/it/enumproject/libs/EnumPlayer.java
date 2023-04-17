1655192e43953e72b440f7622a8408654666f4db
Axiid <devaxiid@gmail.com>
2023-04-17 18:09:19 +0200

    Started EnumPlayer
    Took 2 minutes

--- src/main/java/it/enumproject/libs/EnumPlayer.java
+++ src/main/java/it/enumproject/libs/EnumPlayer.java
@@ -1,7 +1,19 @@
 package it.enumproject.libs;
 
+import org.bukkit.entity.Player;
+
 public class EnumPlayer {
 
     // TODO: Enum player class
+
+    private final Player player;
+
+    public EnumPlayer(Player player) {
+        this.player = player;
+    }
+
+    public void sendTo(String serverName) {
+        // TODO: Send to server
+    }
 
 }
