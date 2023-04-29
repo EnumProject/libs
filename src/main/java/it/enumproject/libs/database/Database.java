1234c5f20f214eef1dd2c56d5f9cbc580a8717af
Axiid <devaxiid@gmail.com>
2023-04-29 18:02:14 +0200

    Some updates

---
+++ src/main/java/it/enumproject/libs/database/Database.java
@@ -0,0 +1,25 @@
+package it.enumproject.libs.database;
+
+import java.sql.Connection;
+
+public interface Database {
+
+    /*
+    Qui ci sarà l'url che andrà utilizzato per connettere il
+    tutto nel database attraverso il metodo Database#connect
+     */
+    String getUrl();
+
+    /*
+    Questo metodo verrà richiamato nell'EnumPlugin per connettere
+    il database all'inizializzazione del plugin.
+     */
+    void connect();
+
+    /*
+    Questo getter servirà solo per farsi ritornare la connection
+    creata nel momento che l'interfaccia viene implementata.
+     */
+    Connection getConnection();
+
+}
